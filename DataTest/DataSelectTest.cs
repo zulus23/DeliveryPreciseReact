925194d2392a08af93e5056525230b58372d23aa
zulus23 <zulus23@gmail.com>
2018-09-19 11:16:00 +0300

    Add test for connection

---
+++ DataTest/DataSelectTest.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using DeliveryPreciseReact.Domain;
+using DeliveryPreciseReact.Service;
+using Xunit;
+
+namespace DataTest
+{
+    public class DataSelectTest
+    {
+        [Fact]
+        public void ListCustomersMustBeFill()
+        {
+            MssqlDataServiceImpl impl = new MssqlDataServiceImpl();
+            List<Customer> customers = impl.ListCustomerByEnterprise("ГОТЭК");
+            Assert.True(customers.Count > 0);
+        }
+    }
+}
