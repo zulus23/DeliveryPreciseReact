9eff51cee603b8cb1ce2a938788293c9b801cf04
zulus23 <zulus23@gmail.com>
2018-10-12 15:20:51 +0300

    Correct component type report

---
+++ DeliveryPreciseReact/wwwroot/Source/components/SelectReport.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+
+class SelectReport extends Component {
+    render() {
+        return (
+            <div className="row">
+               <div className="col-12 p-select-report-drive"> 
+                <label htmlFor="isDriverReport"  >
+                    <input className="align-middle  pb-0" key="1"  id="isDriverReport" 
+                           type="checkbox" value="Движение заказов"
+                           checked={this.props.isDriverReport}
+                           onChange={this.props.onChange} />
+                    <span className="align-text-middle pl-2">Движение заказов</span> 
+                </label>
+               </div>
+               <div className="col-12  p-select-report-kpi">
+                <label htmlFor="isKPIReport"  >
+                    <input className="align-middle pb-0" key="1"  id="isKPIReport" 
+                           type="checkbox" value="KPI"
+                           checked={this.props.isKPIReport}
+                           onChange={this.props.onChange}
+                    />
+                    <span className="align-text-middle pl-2">KPI</span>
+                </label>
+               </div>     
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state){
+    const {isDriverReport,isKPIReport} = state;
+    return {isDriverReport,isKPIReport}
+}
+
+export default connect(mapStateToProps)(SelectReport);
